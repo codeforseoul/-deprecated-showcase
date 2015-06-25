39f54c2effa98b712c23372becdd2badf79b7037
hoony <thechunsik@gmail.com>
2015-06-26 00:53:37 +0900

    create classes in Parse: User, Project and created parseSDK service in angular

--- angular/app/scripts/service/parseSDK.js
+++ angular/app/scripts/service/parseSDK.js
@@ -3,8 +3,83 @@
 angular
   .module('showcaseApp')
     .factory('parseSDK', ['$q', function ($q) {
+      var self = this;
       // Parse App Key: showcase
       Parse.initialize("Z0hMb4HRaZMuDtEPRwKKI6qq0hr06bpH6PGFaxQL", "pfWLen1gM2ySvmHkVaNpdRzKjUGskPGtr85AuI5Q");
 
+      var Project = Parse.Object.extend('Project');
+
+      return {
+        getById: function (className, id, callback) {
+          var cb = callback || angular.noop;
+          var deferred = $q.defer();
+
+          var classObject = Parse.Object.extend(className);
+          var query = new Parse.Query(classObject);
+
+          query.get(id, {
+            success: function (row) {
+              deferred.resolve(row);
+              return cb();
+            },
+            error: function (row,error) {
+              deferred.reject(error);
+              return cb();
+            }
+          });
+
+          return deferred.promise;
+        },
+
+        getAll: function (className, count, callback) {
+          var cb = callback || angular.noop;
+          var deferred = $q.defer();
+
+          var classObject = className == 'User' ? Parse.User : Parse.Object.extend(className);
+          var query = new Parse.Query(classObject);
+
+          // set limit number of rows to get if you want
+          if (count > 0) query.limit(count);
+
+          query.find({
+            success: function (rows) {
+              deferred.resolve(rows);
+              return cb();
+            },
+            error: function (rows, error) {
+              deferred.reject(error);
+              return cb();
+            }
+          });
+
+          return deferred.promise;
+        },
+
+        addMemberToProject: function (userId, projectId, callback) {
+          var self = this;
+          var cb = callback || angular.noop;
+          var deferred = $q.defer();
+
+          var userQ = new Parse.Query(Parse.User);
+          var projectQ = new Parse.Query(Project);
+          self.getById('User', userId)
+            .then(function (user) {
+              self.getById('Project', projectId)
+                .then(function (project) {
+                  var contributors = project.relation('contributors');
+
+                  contributors.add(user);
+                  project.save();
+
+                  deferred.resolve(project);
+                }, function (project, error) {
+                  deferred.reject(error);
+                });
+            }, function (user, error) {
+              deferred.reject(error);
+            });
+
+          return deferred.promise;
+        }
       };
     }]);
